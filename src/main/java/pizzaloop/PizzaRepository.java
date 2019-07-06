4a86856a07aa883e7972c00a90b0605621ace99e
samaliDissanayake <samaliMadushani96@gmil.com>
2019-07-06 18:08:40 +0530

    mysql rest application update

--- src/main/java/pizzaloop/PizzaRepository.java
+++ src/main/java/pizzaloop/PizzaRepository.java
@@ -6,14 +6,10 @@ import javax.transaction.Transactional;
 import java.util.List;
 @Transactional
 public interface PizzaRepository extends CrudRepository<PizzaDetails, Integer>{
-    /*
-    * Find pizza by Id
-    */
+
     List<PizzaDetails> findByPizzaId(Integer id);
 
-    /*
-    * Delete pizza by Id
-    */
+
     List<PizzaDetails> deleteByPizzaId(Integer id);
 }
 
